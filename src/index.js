28a2ba79f32f60823eb974fd1c48c286c37b2857
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-22 13:17:39 -0700

    remove comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import App from './App.svelte'; // importamos el componente App
+import App from './App.svelte';
 
-// Creamos una app que extiende del App compoenente que creamos con la lógica de nuestra aplicación.
 const app = new App({
-	target: document.querySelector('div'), // punto de entrada
-	props: {
-		name: 'Svelte',
-	}, // props de la aplicación
+	target: document.getElementById('root'), // entry point
 });
 
-// Exportamos por defecto app.
 export default app;
