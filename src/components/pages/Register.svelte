11abfbd74152ab914460a7532b769060dbab4b45
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-24 11:29:47 -0700

    refactor(container): move register form to its own file

--- src/components/pages/Register.svelte
+++ src/components/pages/Register.svelte
@@ -1,10 +1,6 @@
 <script>
-	import { link } from 'svelte-routing';
 	import Logo from '../atoms/Logo.svelte';
-	import Input from '../molecules/Input.svelte';
-	let username = '';
-	let email = '';
-	let password = '';
+	import RegisterForm from '../containers/RegisterForm.svelte';
 </script>
 
 <style>
@@ -14,80 +10,9 @@
 		margin-top: 10px;
 		margin-bottom: 30px;
 	}
-	main {
-		background: #242526;
-		box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
-		border-radius: 5px;
-		padding: 10px;
-		margin: 10px;
-	}
-	main h1 {
-		color: white;
-		text-align: center;
-		font-size: 2.4rem;
-		font-weight: bold;
-	}
-	form {
-		display: grid;
-		grid-gap: 14px;
-	}
-	form div {
-		display: flex;
-		justify-content: center;
-		margin-top: 6px;
-	}
-	form div input[type='submit'] {
-		color: white;
-		background: #0779e4;
-		border-radius: 5px;
-		border: none;
-		padding: 10px;
-		width: 100px;
-	}
-	footer {
-		margin-top: 6px;
-	}
-	footer p {
-		text-align: center;
-		font-size: 1.6rem;
-		color: white;
-		margin: 0;
-	}
-	footer p a {
-		text-decoration: none;
-		color: #0779e4;
-	}
 </style>
 
 <header>
 	<Logo />
 </header>
-<main>
-	<h1>Register</h1>
-	<form action="">
-		<Input
-			currentValue="{username}"
-			forInput="username"
-			label="Username"
-			typeInput="text"
-		/>
-		<Input
-			currentValue="{email}"
-			forInput="email"
-			label="Email"
-			typeInput="email"
-		/>
-		<Input
-			currentValue="{password}"
-			forInput="password"
-			label="Password"
-			typeInput="password"
-		/>
-		<div>
-			<input type="submit" value="Register" />
-		</div>
-		<footer>
-			<p>Already have an account? <a href="/login" use:link>Login</a></p>
-		</footer>
-	</form>
-</main>
+<RegisterForm />
