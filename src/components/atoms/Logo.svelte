8e4122e8261d716f79c0e8228ea2dd0bd2d25905
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-23 23:59:01 -0700

    feat(atoms): add logo component

---
+++ src/components/atoms/Logo.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { Link } from 'svelte-routing';
+	import LogoIcon from '../../assets/img/logo.svg';
+	import isAuthenticated from '../../store/auth';
+
+	const isAuth = false;
+
+	isAuthenticated.subscribe((value) => (isAuth = value));
+</script>
+
+<style></style>
+
+<div>
+	<Link to={isAuth ? '/home' : '/'}>
+		<img src={LogoIcon} alt="dummy social network icon" />
+	</Link>
+</div>
