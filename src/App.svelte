2f2d2b4636cacebeba168100e18dc3664b64ed7d
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-24 10:17:11 -0700

    feat(pages): add register form

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
 	import { Router, Route } from 'svelte-routing';
 	import Landing from './components/pages/Landing.svelte';
+	import Register from './components/pages/Register.svelte';
 </script>
 
 <Router>
 	<Route path="/" component="{Landing}" />
+	<Route path="/register" component="{Register}" />
 </Router>
--- src/components/molecules/Input.svelte
+++ src/components/molecules/Input.svelte
@@ -3,18 +3,44 @@
 	export let forInput;
 	export let typeInput = 'text';
 	export let currentValue;
+	export let placeholder;
 
 	function handleTypeInput(e) {
 		e.target.type = typeInput;
 	}
 </script>
 
-<style></style>
+<style>
+	label {
+		display: grid;
+		grid-gap: 10px;
+	}
+	span {
+		color: #eeeeee;
+		font-size: 1.6rem;
+	}
+	input {
+		transition: 0.25s;
+		outline: none;
+		border: 1px solid #3a3b3c;
+		border-radius: 5px;
+		background-color: #3a3b3c;
+		color: white;
+		height: 30px;
+		padding-left: 10px;
+		font-size: 1.6rem;
+	}
+	input:focus {
+		transition: 0.25s;
+		border: 1px solid #0779e4;
+	}
+</style>
 
 <label for="{forInput}">
 	<span>{label}</span>
 	<input
 		id="{forInput}"
+		placeholder="{placeholder}"
 		bind:value="{currentValue}"
 		on:input="{handleTypeInput}"
 	/>
