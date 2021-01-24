8b41110eb04f50575b8231df6859584504f710ff
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-24 09:45:11 -0700

    feat(molecules): add input

---
+++ src/components/molecules/Input.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let label;
+	export let forInput;
+	export let typeInput = 'text';
+	export let currentValue;
+
+	function handleTypeInput(e) {
+		e.target.type = typeInput;
+	}
+</script>
+
+<style></style>
+
+<label for="{forInput}">
+	<span>{label}</span>
+	<input
+		id="{forInput}"
+		bind:value="{currentValue}"
+		on:input="{handleTypeInput}"
+	/>
+</label>
