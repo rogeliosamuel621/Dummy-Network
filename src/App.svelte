11129f6f68e192ab5ef6be32a313a2eda80387d1
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-26 09:04:41 -0700

    feat(pages): add Home

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,12 @@
 	import Landing from './components/pages/Landing.svelte';
 	import Register from './components/pages/Register.svelte';
 	import Login from './components/pages/Login.svelte';
+	import Home from './components/pages/Home.svelte';
 </script>
 
 <Router>
 	<Route path="/" component="{Landing}" />
 	<Route path="/register" component="{Register}" />
 	<Route path="/login" component="{Login}" />
+	<Route path="/home" component="{Home}" />
 </Router>
---
+++ src/components/pages/Home.svelte
@@ -0,0 +1,9 @@
+<script>
+	import isAuthenticated from '../../store/auth';
+	let isAuth = false;
+
+	isAuthenticated.subscribe((value) => (isAuth = value));
+	console.log(isAuth);
+</script>
+
+<h1>Home</h1>
