93a4f451a8441abb75001947604852287375f5a8
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-29 16:15:25 -0700

    feat(api): add user eschema

---
+++ packages/api/src/user/schemas/user.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+
+export type UserDocument = User & mongoose.Document;
+
+@Schema()
+export class User {
+	@Prop({ required: true, maxlength: 33 })
+	username: string;
+
+	@Prop({ required: true, unique: true })
+	email: string;
+
+	@Prop({ required: true, minlength: 6 })
+	password: string;
+
+	@Prop({ required: false, default: '' })
+	profilePic: string;
+
+	@Prop({ required: false, default: '' })
+	bio: string;
+
+	@Prop({ required: false, default: [] })
+	posts: any[];
+
+	@Prop({ required: false, default: [] })
+	followers: mongoose.Schema.Types.ObjectId[];
+
+	@Prop({ required: false, default: [] })
+	following: mongoose.Schema.Types.ObjectId[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
