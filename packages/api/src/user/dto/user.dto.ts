b3ee5556c5d046bab96c94dd4a16b4d3163897da
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-29 23:28:15 -0700

    feat(api/user): add validation to dto

--- packages/api/src/user/dto/user.dto.ts
+++ packages/api/src/user/dto/user.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class RegisterUserDto {
+	@IsNotEmpty()
+	@IsString()
 	username: string;
+
+	@IsEmail()
 	email: string;
+
+	@IsString()
+	@MinLength(6)
 	password: string;
 }
