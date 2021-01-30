18db4161ccd5f681800cb541cd47c28a413db802
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-29 19:53:37 -0700

    feat(api/user): add user model

--- packages/api/src/user/user.module.ts
+++ packages/api/src/user/user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controllers';
 import { UserService } from './user.services';
+import { User, UserSchema } from './schemas/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+	imports: [
+		MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+	],
 	controllers: [UserController],
 	providers: [UserService],
 })
--- packages/api/src/user/user.services.ts
+++ packages/api/src/user/user.services.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { RegisterUserDto } from './dto/user.dto';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
 export class UserService {
+	constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
 	register(user: RegisterUserDto): string {
 		if (user.password.length < 6) {
 			return 'PASSWORD MUST HAVE AT LEAST 6 CHARACTERS';
