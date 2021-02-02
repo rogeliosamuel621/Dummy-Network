58aceb8bc95c4260a05c5d41cb59e3ae1d01b79a
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-02 10:02:34 -0700

    test(api/user): change login tests to their own file

--- packages/api/src/user/__tests__/login.test.ts
+++ packages/api/src/user/__tests__/login.test.ts
@@ -10,7 +10,7 @@ import { UserModule } from '../user.module';
 import { ValidationPipe } from '@nestjs/common';
 import { validationPipesOptions } from '../../utils/validationPipesOptions';
 
-describe('Register endpoint', () => {
+describe('Login endpoint', () => {
 	let app: INestApplication;
 
 	beforeAll(async () => {
@@ -21,6 +21,9 @@ describe('Register endpoint', () => {
 		app = moduleRef.createNestApplication();
 		app.useGlobalPipes(new ValidationPipe(validationPipesOptions));
 		await app.init();
+
+		const mockUser = new FakeUser('admin@gmail.com', 'admin', '123456');
+		await req(app.getHttpServer()).post('/user/register').send(mockUser);
 	});
 	afterAll(async () => {
 		await closeInMongodConnection();
@@ -28,10 +31,10 @@ describe('Register endpoint', () => {
 	});
 
 	test('WRONG DATA SCHEMA', async () => {
-		const mockUser = new FakeUser('admingmail.com', 'admin', '123456');
+		const mockUser = new FakeUser('admingmail.com', '', '123456');
 
 		const res = await req(app.getHttpServer())
-			.post('/user/register')
+			.post('/user/login')
 			.send(mockUser);
 
 		expect(res.status).toBe(400);
@@ -40,25 +43,36 @@ describe('Register endpoint', () => {
 		);
 	});
 
+	test('WRONG EMAIL', async () => {
+		const mockUser = new FakeUser('admin7@gmail.com', '', '123456');
+
+		const res = await req(app.getHttpServer())
+			.post('/user/login')
+			.send(mockUser);
+
+		expect(res.status).toBe(401);
+		expect(res.body.msg).toBe('WRONG EMAIL');
+	});
+
+	test('WRONG PASSWORD', async () => {
+		const mockUser = new FakeUser('admin@gmail.com', '', '1234567');
+
+		const res = await req(app.getHttpServer())
+			.post('/user/login')
+			.send(mockUser);
+
+		expect(res.status).toBe(401);
+		expect(res.body.msg).toBe('PASSWORDS DOES NOT MATCH');
+	});
+
 	test('OK', async () => {
 		const mockUser = new FakeUser('admin@gmail.com', 'admin', '123456');
 
 		const res = await req(app.getHttpServer())
-			.post('/user/register')
+			.post('/user/login')
 			.send(mockUser);
 
-		expect(res.status).toBe(201);
-		expect(res.body.msg).toBe('USER REGISTERED');
-	});
-
-	test('EMAIL ALREADY TAKEN', async () => {
-		const mockUser = new FakeUser('admin@gmail.com', 'admin', '123456');
-
-		const res = await req(app.getHttpServer())
-			.post('/user/register')
-			.send(mockUser);
-
-		expect(res.status).toBe(400);
-		expect(res.body.msg).toBe('EMAIL ALREADY TAKEN');
+		expect(res.status).toBe(200);
+		expect(res.body.msg).toBe('SUCCESS');
 	});
 });
