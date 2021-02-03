93cea52b8653ddef29d7c3e987e45e04adee961c
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-03 16:12:09 -0700

    feat(api/post): add my first middleware

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { LoggerMiddleware } from './middlewares/jwt.middleware';
 
 @Module({
 	imports: [
@@ -14,4 +16,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 	controllers: [],
 	providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer
+			.apply(LoggerMiddleware)
+			.forRoutes('post')
+			// .apply(LoggerMiddleware)
+			// .forRoutes('user')
+			.apply(LoggerMiddleware)
+			.exclude('user/(*)');
+	}
+}
